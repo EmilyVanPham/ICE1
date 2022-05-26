7eb1d212d65d36fb677516c8173d5ea7be8f5962
Thi Thanh Van Pham <pham31@sheridancollege.ca>
2022-05-26 14:36:41 -0400

    First commit Developer: Thi Thanh Van Pham Date: 26 May, 2022

---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -0,0 +1,64 @@
+/* Modifier: Thi Thanh Van Pham
+ * Student ID:...
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author ----
+ */
+public class CardTrick {
+    
+    public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            c.setValue((int) (13 * Math.random()+1));
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(4 * Math.random())]);
+        }
+        
+        //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner (System.in);
+        System.out.printf("%40s","Please enter the card value from 1-13: ");
+        int cardValue;
+        try{
+            cardValue = input.nextInt();
+        } catch(InputMismatchException e) {
+            System.out.println("Error! You must enter number from 1 to 13!");
+            return;
+        }
+        
+        System.out.printf("%40s","Please enter 0 for Hearts, 1 for Diamonds"
+                + "2 for Spades and 3 for Clubs: ");
+        int cardSuitNumber;
+        try{
+            cardSuitNumber = input.nextInt();
+        } catch(InputMismatchException e) {
+            System.out.println("Error! You must enter number from 0 to 3!");
+            return;
+        }
+        
+        String cardSuit = Card.SUITS[cardSuitNumber];
+        System.out.println("Your card is " + cardValue +" " + cardSuit);
+        
+        // and search magicHand here
+        for (int i=0; i<magicHand.length; i++)  {
+            Card tempCard = magicHand[i];
+            if(tempCard.getValue() == cardValue && tempCard.getSuit().equalsIgnoreCase(cardSuit));
+        }
+//Then report the result here
+
+    }
+    
+}
